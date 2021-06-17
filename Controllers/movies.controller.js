f0100b71afb12befd77eb7d3ad5393b6ed4ffaab
root <root@DESKTOP-H8Q20A4.localdomain>
2021-06-17 15:02:48 +0300

    solve lab 10

--- Controllers/movies.controller.js
+++ Controllers/movies.controller.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
 const axios = require('axios');
-const Movie=require('../Models/movie.model')
+const Movie = require('../Models/movie.model')
 
-const movieController=(req, res)=> {
-    let inputUser= req.query.query;
-    let moviekey = process.env.MOVIE_API_KEY;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${moviekey}&query=${inputUser}`;
-    axios.get(url).then(result =>{
-        const movieArray = result.data.results.map(item=>{
-        return new Movie (item);
-        })
-    res.send(movieArray);
+const movieInfo = {};
+const movieController = (req, res) => {
+  let inputUser = req.query.query;
+  let movieKey = process.env.MOVIE_API_KEY;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=${inputUser}`;
+  if (movieInfo[inputUser] !== undefined) {
+    console.log('get from Memory');
+    res.send(movieInfo[inputUser]);
+  } else {
+    axios.get(url).then(result => {
+      const movieArray = result.data.results.map(item => {
+        return new Movie(item);
+      });
+      movieInfo[inputUser] = movieArray;
+      res.send(movieArray);
     })
-    .catch(error =>{
-      res.send(`there is an error in getting the data => ${error}`);
+    .catch ((error) => {
+  res.send(`there is an error in getting the data => ${error}`);
     })
-  }
-
-  module.exports=movieController;
+}
+}
+module.exports = movieController;
